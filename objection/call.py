28d792fcab582f38e59569b0463bb256e184624c
peach lasagna <kir.kud@inbox.ru>
2021-04-18 18:58:10 +0700

    comma

--- objection/call.py
+++ objection/call.py
@@ -1,27 +1,71 @@
 import ast
 import inspect
 from typing import Callable
+import re
 
+MATCH_PACK = r'(?<=site-packages\/).*(?=\.\w+$)'
+_list = []
+PATH = ""
+
+def solve_name(node):
+    if isinstance(node, ast.Call):
+        fu = node.func
+        return solve_name(fu), node.args
+    elif isinstance(node, ast.Name):
+        return node.id
+    elif isinstance(node, ast.Attribute):
+        return solve_name(node.value) + '.' + node.attr
+
+def imp_com(path: str, name: str):
+    if '.' in name:
+        exc = f"from {path} import {name.split('.')[0]}"
+    else:
+        exc = f"from {path} import {name}"
+    print(exc)
+    exec(exc)
 
 class CallExplorer:
     def __init__(self):
-        pass
+        self.space =  '    '
+        self.branch = '│   '
+        self.tee =    '├── '
+        self.last =   '└── '
 
     def call_tree(self, func: Callable):
-        ast.parse(inspect.getsource(func))
-
-
+        global PATH
+        _list = []
+        visitor = CallVisitor()
+        tree = ast.parse(inspect.getsource(func))
+        PATH = inspect.getsourcefile(func)
+        PATH = re.search(MATCH_PACK, PATH).group().replace('/', '.')
+        imp_com(PATH, func.__name__)
+        tree = visitor.visit(tree)
+        return _list
 
 
 class CallVisitor(ast.NodeVisitor):
-    def visit_Call(self, node):
-        print(node.__dict__)
+    def visit_Call(self, node: ast.Call):
+        global PATH
+        # try:
+        name, args = solve_name(node)
+        print(name)
+        imp_com(PATH, name)
+        _list.append((name, args))
+        obj = exec(name)
+        PATH = inspect.getsourcefile(obj)
+        PATH = re.search(MATCH_PACK, PATH).group().replace('/', '.')
+        self.visit(ast.parse(inspect.getsource(obj)))
+        # except Exception as e: print(e)
+        # finally:
         return node
-
 """
 >>> Functionname
-f1 ()
-
-
+|-f1 ()
+  |_ f3(args)
 
 """
+from fastapi import Depends
+
+
+
+print(CallExplorer().call_tree(Depends))
